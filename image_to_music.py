6b20287c6d22864051fe5a5927bfe797d3a8d62d
Fedi Nabli <fedinabli@gmail.com>
2023-02-06 17:28:28 +0100

    Added song generation function

--- image_to_music.py
+++ image_to_music.py
@@ -113,3 +113,100 @@ def hue2freq(h, scale_freqs):
 
   return note
 
+# Make song from image
+def img2music(img, scale = [220.00, 246.94 ,261.63, 293.66, 329.63, 349.23, 415.30],
+              sr = 22050, T = 0.1, n_pixels = 60, use_octaves = True, random_pixels = False,
+              harmonize = 'U0'):
+  """
+  Args:
+    img    :     (array) image to process
+    scale  :     (array) array containing frequencies to map H values to
+    sr     :     (int) sample rate to use for resulting song
+    T      :     (int) time in seconds for dutation of each note in song
+    n_pixels:     (int) how many pixels to use to make song
+  Returns:
+    song   :     (array) Numpy array of frequencies. Can be played by ipd.Audio(song, rate = sr)
+  """
+  # Convert image to HSV
+  hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+  # Get shape of image
+  height, width, depth = img.shape
+
+  i = 0; j = 0 ; k = 0
+  # Initialize array that will contain Hues for every pixel in image
+  hues = []
+  if random_pixels == False:
+    for val in range(n_pixels):
+      hue = abs(hsv[i][j][0]) # This is the hue value at pixel coordinate (i,j)
+      hues.append(hue)
+      i += 1
+      j += 1
+
+  else:
+    for val in range(n_pixels):
+      i = random.randint(0, height-1)
+      j = random.randint(0, width-1)
+      hue = abs(hsv[i][j][0])
+      hues.append(hue)
+
+  # Make dataframe containing hues and frequencies
+  pixels_df = pd.DataFrame(hues, columns=['hues'])
+  pixels_df['frequencies'] = pixels_df.apply(lambda row : hue2freq(row['hues'], scale), axis=1)
+  frequencies = pixels_df['frequencies'].to_numpy()
+
+  # Convert frequency to a note
+  pixels_df['notes'] = pixels_df.apply(lambda row : librosa.hz_to_note(row['frequencies']), axis=1) # type: ignore
+
+  # Convert note to a midi number
+  pixels_df['midi_number'] = pixels_df.apply(lambda row : librosa.hz_to_midi(row['notes']), axis=1)
+
+  # Make harmony disctionary
+  # unison           = U0 ; semitone        = ST ; major second     = M2
+  # minor third      = m3 ; major third     = M3 ; perfect forth    = P4
+  # diatonic tritone = DT ; perfect fifth   = P5 ; minor sixth      = m6
+  # major sixth      = M6 ; minor seventh   = m7 ; major seventh    = M7
+  # octave           = O8
+  harmony_select = {'U0': 1,
+                    'ST': 16/15,
+                    'M2': 9/8,
+                    'm3': 6/5,
+                    'M3': 5/4,
+                    'P4': 4/3,
+                    'DT': 45/32,
+                    'P5': 3/2,
+                    'm6': 8/5,
+                    'M6': 5/3,
+                    'm7': 9/5,
+                    'M7': 15/8,
+                    'O8': 2}
+  
+  harmony = np.array([]) # This array will contain the song harmony
+  harmony_val = harmony_select[harmonize] # This will select the ration for the desired harmony
+
+  song = np.array([]) # This array will contain the chosen frquencies in our song
+  octaves = np.array([0.5, 1, 2]) # Go an octave below, same song or an octave higher
+  t = np.linspace(0, T, int(T*sr), endpoint=False) # time variable
+
+  # Make a sing with numpy array
+  for k in range(n_pixels):
+    if use_octaves:
+      octave = random.choice(octaves)
+    else:
+      octave = 1
+    
+    if random_pixels == False:
+      val = octave * frequencies[k]
+    else:
+      val = octave * random.choice(frequencies)
+    
+    # Make note and hormony note
+    note = 0.5*np.sin(2*np.pi*val*t)
+    h_note = 0.5*np.sin(2*np.pi*harmony_val*val*t)
+
+    # Place notes into corresponding arrays
+    song = np.concatenate([song, note])
+    harmony = np.concatenate([harmony, h_note])
+
+  return song, pixels_df, harmony
+
